573e95a842d69dc21e882f2d12b71925ce531e44
Gkart <rohit.shinde@walchandsangli.ac.in>
2023-09-14 04:51:02 +0530

    modified config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  publicPath: '/', // Set the publicPath to '/'
+  publicPath: '/',
   transpileDependencies: true,
+
+  // Add a devServer configuration to allow host headers
+  devServer: {
+    disableHostCheck: true,
+  },
 });
